b68fbd4afb35c0f3348822303e7ed0676009e819
codeposse <codeposse@gmail.com>
2020-05-01 13:15:28 -0700

    Vanilla JS :construction:

--- JS/WCAG_AA_Vanilla.js
+++ JS/WCAG_AA_Vanilla.js
@@ -24,37 +24,43 @@ document.querySelector('img:not([alt])').setAttribute('alt', 'my alt text');
 /*add alt to first image with an alt but no value*/
 document.querySelector("img:not([alt=''])").setAttribute('alt', 'my alt text');
 
-/*add to all images, computationally heavy*/
+/*add to all images regardless*/
 var y = document.querySelectorAll("img");
 var i;
 for (i = 0; i < y.length; i++) {
-  y[i].setAttribute("alt", "my alt text");
+    y[i].setAttribute("alt", "my alt text");
 }
 
 /*same but for missing tag*/
 var y = document.querySelectorAll('img:not([alt])');
 var i;
 for (i = 0; i < y.length; i++) {
-  y[i].setAttribute("alt", "my alt text");
+    y[i].setAttribute("alt", "my alt text");
 }
 
 /*add to a specific img with an ID*/
-var x = document.getElementById("myAnchor"); 
-if (!x.hasAttribute("alt")) {      
+var x = document.getElementById("myAnchor");
+if (!x.hasAttribute("alt")) {
     x.setAttribute("alt", "my alt text");
 }
 
 /*add to the first image on a page, and it doesn't have an alt*/
-var x = document.getElementsByTagName("img")[0]; 
-if (!x.hasAttribute("alt")) {      
+var x = document.getElementsByTagName("img")[0];
+if (!x.hasAttribute("alt")) {
     x.setAttribute("alt", "my alt text");
 }
 
+/*remove titles from images*/
+var y = document.querySelectorAll("img");
+var i;
+for (i = 0; i < y.length; i++) {
+    y[i].removeAttribute("title");
+}
 
 //EMBEDED MEDIA//
 /*add to iframes without titles*/
 var y = document.querySelectorAll('iframe:not([title])');
 var i;
 for (i = 0; i < y.length; i++) {
-  y[i].setAttribute("title", "my title text");
+    y[i].setAttribute("title", "my title text");
 }
