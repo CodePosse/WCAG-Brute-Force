f99c700c78108eb9d659fc3b4a2de9c0bb0e7dba
codeposse <codeposse@gmail.com>
2020-06-22 14:18:33 -0700

    early theme switcher

---
+++ Style Switcher/JS/styleswap_jQuery.js
@@ -0,0 +1,8 @@
+/* lame but effective, used onclick */
+$.fn.themeWCAG = function () {
+    $('link').attr("href", './../CSS/theme-wcag.css');
+}; 
+
+$.fn.themeDefault = function () {
+    $('link').attr("href", './../CSS/theme.css');
+}; 
