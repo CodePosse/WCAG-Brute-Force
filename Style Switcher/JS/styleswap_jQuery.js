81b347002406773f49b779dbc76fe6e11836eef8
T.Hunold <codeposse@gmail.com>
2026-01-27 11:35:27 -0800

    :wheelchair: WCAG/ADA jQuery and VanillaJS style switchers

--- Style Switcher/JS/styleswap_jQuery.js
+++ Style Switcher/JS/styleswap_jQuery.js
@@ -1,8 +1,20 @@
 /* lame but effective, used onclick */
 $.fn.themeWCAG = function () {
     $('link').attr("href", './../CSS/theme-wcag.css');
-}; 
+};
 
 $.fn.themeDefault = function () {
     $('link').attr("href", './../CSS/theme.css');
-}; 
+};
+//add it to the a link itself
+$.fn.themeToggle = function () {
+    const isDefault =
+        $('#theme').attr('href') === './../CSS/theme-wcag.css';
+
+    if (isDefault) {
+        $(this).themeDefault();
+    } else {
+        $(this).themeWCAG();
+    }
+};
+
