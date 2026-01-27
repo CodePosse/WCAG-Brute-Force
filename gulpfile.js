c9af9add3b4769005405de5e0b106187bdbb40b8
T.Hunold <codeposse@gmail.com>
2026-01-27 13:02:49 -0800

    :sparkles: update comments to emphasize source and destination placeholders in gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -22,7 +22,7 @@ gulp.task("default", async function () {
 
 /* to run, type: gulp IMG-FIX */
 gulp.task('IMG-FIX', async function () {
-  gulp.src('src/html/*.html') //DUDE, CHANGE ME TO YOUR SOURCE
+  gulp.src('src/html/*.html') //DUDE, CHANGE ME TO *YOUR* SOURCE
     .pipe(cheerio(function ($, file) {
       $("img:not([alt])").attr("alt", "my new alt"); //this adds a blank alt tag to images without an alt
       $("img([alt=''])").attr("alt", "my new alt"); //this adds a value to an image alt without one
@@ -33,19 +33,19 @@ gulp.task('IMG-FIX', async function () {
 
 /* to run, type: gulp IFRAME-FIX */
 gulp.task('IFRAME-FIX', async function () {
-  gulp.src('src/html/*.html') //DUDE, CHANGE ME TO YOUR SOURCE
+  gulp.src('src/html/*.html') //DUDE, CHANGE ME TO *YOUR* SOURCE
     .pipe(cheerio(function ($, file) {
       $('iframe:not([title])').attr({ title: 'your iframe title' }); //this adds a title to an iframe without one
       $('iframe([title=""])').attr({ title: 'your iframe title' }); //this adds a value to an iframe title without one
     }))
-    .pipe(gulp.dest('./prod/html/')); //DUDE, CHANGE ME TO YOUR DESTINATION
+    .pipe(gulp.dest('./prod/html/')); //DUDE, CHANGE ME TO *YOUR* DESTINATION
 });
 
 
 /* to run, type: gulp Brute-Force */
 /* this one is a work in progress, super super dangerous if you are not prepared */
 gulp.task('Brute-Force', async function () {
-  gulp.src('src/html/*.html') //DUDE, CHANGE ME TO YOUR SOURCE
+  gulp.src('src/html/*.html') //DUDE, CHANGE ME TO *YOUR* SOURCE
     .pipe(cheerio(function ($, file) {
       $('iframe:not([title])').attr({ title: 'your iframe title' }); //this adds a title to an iframe without one
       $('iframe([title=""])').attr({ title: 'your iframe title' }); //this adds a value to an iframe title without one
@@ -63,20 +63,26 @@ gulp.task('Brute-Force', async function () {
         $(this).removeAttr("placeholder");
         $(this).before("<label for=" + id + ">" + ph + ":</label>");
       });
+      $('html:not([lang])').attr('lang', 'en-US'); //declare default lang attribute
+      //force doctype and lang attribute
+      // this is only needed if the doctype is missing
+      if (document.doctype == null) {
+        $('html').before('<!DOCTYPE html>');
+      }
     }))
-    .pipe(gulp.dest('./prod/html/')); //DUDE, CHANGE ME TO YOUR DESTINATION
+    .pipe(gulp.dest('./prod/html/')); //DUDE, CHANGE ME TO *YOUR* DESTINATION
 });
 
 /* to run, type: gulp PDF-FIX */
 gulp.task('PDF-FIX', async function () {
-  gulp.src('src/html/*.html') //DUDE, CHANGE ME TO YOUR SOURCE
+  gulp.src('src/html/*.html') //DUDE, CHANGE ME TO *YOUR* SOURCE
     .pipe(cheerio(function ($, file) {
       $('a[href$=".pdf"]').each(function () {
         var linktext = $(this).text();
         $(this).attr("aria-label", linktext + " (opens PDF document)");
       });
     }))
-    .pipe(gulp.dest('./prod/html/')); //DUDE, CHANGE ME TO YOUR DESTINATION
+    .pipe(gulp.dest('./prod/html/')); //DUDE, CHANGE ME TO *YOUR* DESTINATION
 });
 
 // add ARIA labels to document links for doc and docx files
